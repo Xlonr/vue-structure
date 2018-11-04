594072f5ec9ee064ca58c131ac194559d22c783f
xlon <873685744@qq.com>
2018-11-04 17:01:43 +0800

    unuse router

--- router/index.js
+++ router/index.js
@@ -4,7 +4,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import AppPage from '../src/components/app.vue'
-import Second from '../src/components/classify/second.vue'
+// import Second from '../src/components/classify/second.vue'
 import vBtn from '../src/view/v_btn.vue'
 import vCheckBox from '../src/view/v_checkBox.vue'
 import vInput from '../src/view/v_input.vue'
